7e3bdbd30cfafb6ff7006bed2a208945a25a1233
JohanTv <johan.tanta@utec.edu.pe>
2023-11-04 23:26:01 -0500

    move and refactor: antispoofing_models.* to models/ folder, refactor code (app.py, check_faces.py) and add faces/ folder

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 import cv2
-from tensorflow.keras.preprocessing.image import img_to_array
 import os
 import numpy as np
-from tensorflow.keras.models import model_from_json
 
-root_dir = os.getcwd()
+from tensorflow import keras
+from keras.models import model_from_json
+
 # Load Face Detection Model
 face_cascade = cv2.CascadeClassifier("models/haarcascade_frontalface_default.xml")
 # Load Anti-Spoofing Model graph
-json_file = open('antispoofing_models/antispoofing_model.json','r')
+json_file = open('models/antispoofing_model.json','r')
 loaded_model_json = json_file.read()
 json_file.close()
 model = model_from_json(loaded_model_json)
 # load antispoofing model weights 
-model.load_weights('antispoofing_models/antispoofing_model.h5')
+model.load_weights('models/antispoofing_model.h5')
 print("Model loaded from disk")
 # video.open("http://192.168.1.101:8080/video")
 # vs = VideoStream(src=0).start()
@@ -29,7 +29,6 @@ while True:
             face = frame[y-5:y+h+5,x-5:x+w+5]
             resized_face = cv2.resize(face,(160,160))
             resized_face = resized_face.astype("float") / 255.0
-            # resized_face = img_to_array(resized_face)
             resized_face = np.expand_dims(resized_face, axis=0)
             # pass the face ROI through the trained liveness detector
             # model to determine if the face is "real" or "fake"
@@ -53,5 +52,6 @@ while True:
             break
     except Exception as e:
         pass
+
 video.release()        
 cv2.destroyAllWindows()
